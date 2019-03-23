f2569d231751d111eb56a2f07bf199c17690433e
Bri0927 <43020510+Bri0927@users.noreply.github.com>
2019-03-23 15:17:42 -0700

    Class Group Defined v1

---
+++ group.js
@@ -0,0 +1,13 @@
+
+
+class Group() {
+  constructor(time, location, description, reoccur, announcements, listOfStudents, creator) {
+    this.time = time;
+    this.location = location;
+    this.description = description;
+    this.reoccur = reoccur;
+    this.announcements = announcements;
+    this.listOfStudents = listOfStudents;
+    this.creator = creator;
+  }
+}
