e9ff78a8d3de0d195000bfccf57153091b66174f
Bri0927 <43020510+Bri0927@users.noreply.github.com>
2019-03-23 18:00:25 -0700

    Created a test file v1

---
+++ madhat-study/src/client/classes/group.js
@@ -0,0 +1,23 @@
+
+
+class Group() {
+  constructor(time, location, description, reoccur, announcements, listOfStudents, creator) {
+    this.time = time;
+    this.location = location;
+    this.description = description;
+    this.reoccur = reoccur;
+    this.announcements = announcements;
+    this.listOfStudents = listOfStudents;
+    this.creator = creator;
+    this.setTime() {
+      return;
+    }
+    this.setLocation() {
+      return;
+    }
+    this.setDescription(){
+      return;
+    }
+    }
+  }
+}
