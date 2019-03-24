ec6af878186f3be01f65af9e384c7f556a8ad7da
Connor <cwr1998@rocketmail.com>
2019-03-24 00:12:57 -0700

    NavBar Testing

--- madhat-study/src/client/study-buddy.js
+++ madhat-study/src/client/study-buddy.js
@@ -2,5 +2,6 @@ import React from "react";
 import {reactRender} from "@ombiel/aek-lib";
 
 import Screen from "./study-buddy/screen";
+import Screen2 from "./study-buddy/screen2";
 
 reactRender(<Screen />);
