f4b4c69fa6642e2f12e1174ae307f6aa9e58e985
Emily Feay <40258331+eafeay@users.noreply.github.com>
2019-03-23 22:17:44 -0700

    Updated UI

--- madhat-study/src/client/study-buddy/screen.js
+++ madhat-study/src/client/study-buddy/screen.js
@@ -2,9 +2,13 @@ import React from "react";
 import {
   Container,
   VBox,
+  HBox,
   BannerHeader,
   BasicSegment,
-  Button,<
+  Button,
+  Header,
+  Listview,
+  ListviewItem as Item,
 } from "@ombiel/aek-lib";
 
 function selectUser() {
@@ -26,21 +30,114 @@ function selectUser() {
 
 function sDisplay (//etc)
 {
-    export default Screen extends React.Component {
+     export default Screen extends React.Component {
         render() {
 
-	    return (
-	    
-	    );
-	}
+          return (
+            <Container>
+              <VBox>
+                <BanerHeader theme="alt" key="header" dataflex={0}>Cougs Study</BannerHeader>
+                <BasicSegment>
+                    <HBox>
+                      <Panel />
+                      <Header>"Current Schedule"</Header>
+                        <div>
+                          <Listview>
+                            <Item href="#">"Class1"</Item>
+                            <Item href="#">"Class2"</Item>
+                            <Item href="#">"Class3"</Item>
+                          </Listview>
+                        </div>
+                    </HBox>
 
+                    <HBox>
+                      <Panel />
+                      <Header>"Welcome to your dashboard!"</Header>
+                      <CBox>
+                        <Header>"Your Study Groups"</Header>
+                          <div>
+                            <Listview>
+                              <Item href="#">"Group1"</Item>
+                              <Item href="#">"Group2"</Item>
+                              <Item href="#">"Group3"</Item>
+                            </Listview>
+                          </div>
+                      </CBox>
+                    </HBox>
+
+                    <HBox>
+                      <Panel />
+                      <Header>"Announcements"</Header>
+                        <div>
+                          <Listview>
+                            <Item href="#">"A1"</Item>
+                            <Item href="#">"A2"</Item>
+                            <Item href="#">"A3"</Item>
+                          </Listview>
+                        </div>
+                    </HBox>
+                </BasicSegment>
+              </VBox>
+            </Container>
+          );
+        }
     }
-
 }
 
 function pDisplay(//etc)
 {
+     export default Screen extends React.Component {
+        render() {
 
+          return (
+            <Container>
+              <VBox>
+                <BanerHeader theme="alt" key="header" dataflex={0}>Cougs Study</BannerHeader>
+                <BasicSegment>
+                    <HBox>
+                      <Panel />
+                      <Header>"Current Schedule"</Header>
+                        <div>
+                          <Listview>
+                            <Item href="#">"Class1"</Item>
+                            <Item href="#">"Class2"</Item>
+                            <Item href="#">"Class3"</Item>
+                          </Listview>
+                        </div>
+                    </HBox>
+
+                    <HBox>
+                      <Panel />
+                      <Header>"Welcome to your dashboard!"</Header>
+                      <CBox>
+                        <Header>"Your Study Groups"</Header>
+                          <div>
+                            <Listview>
+                              <Item href="#">"Group1"</Item>
+                              <Item href="#">"Group2"</Item>
+                              <Item href="#">"Group3"</Item>
+                            </Listview>
+                          </div>
+                      </CBox>
+                    </HBox>
+
+                    <HBox>
+                      <Panel />
+                      <Header>"Announcements"</Header>
+                        <div>
+                          <Listview>
+                            <Item href="#">"A1"</Item>
+                            <Item href="#">"A2"</Item>
+                            <Item href="#">"A3"</Item>
+                          </Listview>
+                        </div>
+                    </HBox>
+                </BasicSegment>
+              </VBox>
+            </Container>
+          );
+        }
+    }
 }
 
 export default class Screen extends React.Component {
